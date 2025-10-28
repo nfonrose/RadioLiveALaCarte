3871d6f2ab055d7f73d56be3547dab9f057b96df
Nicolas Fonrose <nicolas.fonrose@teevity.com>
2025-10-28 17:31:43 +0100

    test - Update some Stream URLs in the test data

--- rlalc-backend-media-capture-service/src/test/java/com/prtlabs/rlalc/backend/mediacapture/services/configuration/MediaCapturePlanningHelperTest.java
+++ rlalc-backend-media-capture-service/src/test/java/com/prtlabs/rlalc/backend/mediacapture/services/configuration/MediaCapturePlanningHelperTest.java
@@ -43,7 +43,7 @@ public class MediaCapturePlanningHelperTest {
         MediaCapturePlanningDTO.StreamToCapture stream1 = streams.get(0);
         assertEquals("France Inter", stream1.getTitle(), "First stream title does not match");
         assertEquals("7bf97a79-9612-411d-966b-657b6d77443e", stream1.getUuid(), "First stream UUID does not match");
-        assertEquals("https://radiofrance.com/stream/franceinter", stream1.getStreamurl(), "First stream URL does not match");
+        assertEquals("http://direct.franceinter.fr/live/franceinter-midfi.mp3", stream1.getStreamurl(), "First stream URL does not match");
         assertEquals("1761605906", stream1.getStartTimeUTCEpochSec(), "First stream start time does not match");
         assertEquals("1200", stream1.getDurationSeconds(), "First stream duration does not match");
 
@@ -51,7 +51,7 @@ public class MediaCapturePlanningHelperTest {
         MediaCapturePlanningDTO.StreamToCapture stream2 = streams.get(1);
         assertEquals("Europe 1", stream2.getTitle(), "Second stream title does not match");
         assertEquals("d84c5a96-5839-4d27-b033-c5f742832982", stream2.getUuid(), "Second stream UUID does not match");
-        assertEquals("https://europe1.fr/stream", stream2.getStreamurl(), "Second stream URL does not match");
+        assertEquals("https://europe1.lmn.fm/europe1.mp3", stream2.getStreamurl(), "Second stream URL does not match");
         assertEquals("1761605906", stream2.getStartTimeUTCEpochSec(), "Second stream start time does not match");
         assertEquals("1200", stream2.getDurationSeconds(), "Second stream duration does not match");
     }
