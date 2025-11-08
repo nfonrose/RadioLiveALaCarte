01741a4ff9202b06ead9ad0b1b9c6580ef33c1aa
Nicolas Fonrose <nicolas.fonrose@teevity.com>
2025-11-08 15:57:52 +0100

    refactor - Bring back some of the legacy folders (docs, demo, r&d and the GroovyMorningFM iOS app)

--- @legacy/1.RadioLiveALaCarteApp/src/app/service/Model/error/RadioLiveErrorInterface.ts
+++
@@ -1,7 +0,0 @@
-export interface RadioLiveErrorInterface {
-    prtErrorUUID: string;
-    prtErrorCode: string;
-    prtUserErrorMessage: string;
-    prtErrorMessage: string;
-    prtErrorDetails: any[]; // ou un type plus spécifique si connu
-}
